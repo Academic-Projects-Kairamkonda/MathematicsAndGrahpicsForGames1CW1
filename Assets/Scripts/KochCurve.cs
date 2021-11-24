3ac0c0f176ba176bf06f7c7619832b4015227872
Sravan Kumar K <35738653+SravanGameDev@users.noreply.github.com>
2021-11-24 19:35:04 +0000

    L Systems Generate

---
+++ Assets/Scripts/KochCurve.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LindenmayerSystem
+{
+    public class KochCurve : MonoBehaviour
+    {
+        public string axiom = "F";
+
+        public string nameVar = "F";
+
+        private void Start()
+        {
+            
+
+        }
+    }
+}
--- Assets/Scripts/LSystems.cs
+++ Assets/Scripts/LSystems.cs
@@ -3,96 +3,103 @@ using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
 
-public class TransformInfo
+namespace LindenmayerSystem
 {
-    public Vector3 position;
-    public Quaternion rotation;
-}
 
-public class LSystems : MonoBehaviour
-{
-    [SerializeField] private int iteration=4;
-    [SerializeField] private GameObject Branch;
-    [SerializeField] private float length=10f;
-    [SerializeField] private float angle = 30f;
-
-    private const string axiom = "X";
-
-    private Stack<TransformInfo> transformStack;
-    private Dictionary<char, string> rules;
-    private string currentString = string.Empty;
-
-    void Start()
+    public class TransformInfo
     {
-        transformStack = new Stack<TransformInfo>();
-
-        rules = new Dictionary<char, string>
-        {
-           {'X', "[FX][-FX][+FX]" },
-           {'F',"FF" }
-        };
-
-        Generate();
+        public Vector3 position;
+        public Quaternion rotation;
     }
 
-    private void Generate()
+    public class LSystems : MonoBehaviour
     {
-        currentString = axiom;
+        [SerializeField] [Range(0, 4)] private int iteration = 4;
+        [SerializeField] private GameObject Branch;
+        [SerializeField] private float length = 10f;
+        [SerializeField] private float angle = 30f;
 
-        StringBuilder sb = new StringBuilder();
+        private const string axiom = "X";
 
-        for (int i = 0; i < iteration; i++)
+        private Stack<TransformInfo> transformStack;
+
+        // Rules are mentioned 
+        [SerializeField] public Dictionary<char, string> rules;
+
+        //An empty string to load the axiom before iteration
+        private string currentString = string.Empty;
+
+        void Start()
         {
-            foreach (var c in currentString)
-            {
-                sb.Append(rules.ContainsKey(c) ? rules[c] : c.ToString());
-            }
+            transformStack = new Stack<TransformInfo>();
 
-            currentString = sb.ToString();
+            rules = new Dictionary<char, string>
+            {
+               {'X', "[FX][-FX][+FX]"},
+               {'F',"FFF" }
+            };
+
+            Generate(axiom);
         }
 
-
-        foreach (var c in currentString)
+        public void Generate(string s)
         {
-            switch (c)
+            currentString = s;
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < iteration; i++)
             {
-                case 'F':
-                    Vector3 initialPosition = transform.position;
-                    transform.Translate(Vector3.up * length);
+                foreach (var c in currentString)
+                {
+                    sb.Append(rules.ContainsKey(c) ? rules[c] : c.ToString());
+                }
 
-                    GameObject treeSegment = Instantiate(Branch);
-                    treeSegment.GetComponent<LineRenderer>().SetPosition(0, initialPosition);
-                    treeSegment.GetComponent<LineRenderer>().SetPosition(1, transform.position);
-                        break;
-                case 'X':
-
-                case '+':
-                    transform.Rotate(Vector3.back * angle);
-                    break;
-
-                case '-':
-                    transform.Rotate(Vector3.forward * angle);
-                     break;
-
-                case '[':
-                    transformStack.Push(new TransformInfo()
-                    {
-                        position = transform.position,
-                        rotation = transform.rotation
-                    });
-                    break;
-
-                case ']':
-                    TransformInfo ti = transformStack.Pop();
-                    transform.position = ti.position;
-                    transform.rotation = ti.rotation;
-                    break;
-
-                default:
-                    
-                    break;
+                currentString = sb.ToString();
             }
 
+
+            foreach (var c in currentString)
+            {
+                switch (c)
+                {
+                    case 'F':
+                        Vector3 initialPosition = transform.position;
+                        transform.Translate(Vector3.up * length);
+
+                        GameObject treeSegment = Instantiate(Branch);
+                        treeSegment.GetComponent<LineRenderer>().SetPosition(0, initialPosition);
+                        treeSegment.GetComponent<LineRenderer>().SetPosition(1, transform.position);
+                        break;
+                    case 'X':
+
+                    case '+':
+                        transform.Rotate(Vector3.back * angle);
+                        break;
+
+                    case '-':
+                        transform.Rotate(Vector3.forward * angle);
+                        break;
+
+                    case '[':
+                        transformStack.Push(new TransformInfo()
+                        {
+                            position = transform.position,
+                            rotation = transform.rotation
+                        });
+                        break;
+
+                    case ']':
+                        TransformInfo ti = transformStack.Pop();
+                        transform.position = ti.position;
+                        transform.rotation = ti.rotation;
+                        break;
+
+                    default:
+                        Debug.LogWarning("Error found");
+                        break;
+                }
+            }
         }
     }
 }
